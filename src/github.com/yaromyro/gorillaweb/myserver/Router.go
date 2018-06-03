8eac4cb62756764ab2bb40aaa8083fd33c06eb0f
Yaromyr <oryshchyn97@gmail.com>
2018-06-03 10:49:58 +0300

    changes

--- src/github.com/yaromyro/gorillaweb/myserver/Router.go
+++ src/github.com/yaromyro/gorillaweb/myserver/Router.go
@@ -6,9 +6,9 @@ import (
 
 func MakeRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/user/", createUser).Methods("POST")
+	r.HandleFunc("/user", createUser).Methods("POST")
 	r.HandleFunc("/user/{id}", getUser).Methods("GET")
-	r.HandleFunc("/users/", getUsers).Methods("GET")
+	r.HandleFunc("/users", getUsers).Methods("GET")
 	r.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
 	r.HandleFunc("/user/{id}", updateUser).Methods("PUT")
 	return r
